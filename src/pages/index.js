86b5fd219388e4a74593d6df4fcd76f02d701ea0
Scott <agentwinburn1@gmail.com>
2020-10-19 12:21:16 +0800

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,16 +27,15 @@ class IndexPage extends React.Component {
           This is a place where I, Coach Scott, share my learnings and help others like you reach their peak health.
           You can expect topics as they relate to health and wellness on:
 
-          Sport Psychology - Neuroscience - Mindset
-          Fitness - Posture - Optimal human movement
-          Injury Prevention
-          Self Care - Healing
-          Orthopedic Rehabilitation
-          Epigenetics
-          Debunking myths
-          Proven techniques to heal yourself and others
-          Real stories and so much more!
-          
+          <br>Sport Psychology - Neuroscience - Mindset</br>
+          <br>Fitness - Posture - Optimal human movement</br>
+          <br>Injury Prevention</br>
+          <br>Self Care - Healing</br>
+          <br>Orthopedic Rehabilitation</br>
+          <br>Epigenetics</br>
+          <br>Debunking myths</br>
+          <br>Proven techniques to heal yourself and others</br>
+          <br>Real stories and so much more!</br>
           </p>
 
         <p>Let's go build something great and reach our peak!</p>
