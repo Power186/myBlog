b424a4e62239f3c8c19d5342a8996e9bb3dfa292
Scott <agentwinburn1@gmail.com>
2020-10-19 12:08:30 +0800

    home page clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,9 +25,20 @@ class IndexPage extends React.Component {
         <p>Welcome to Conquer Peak Health. I help anyone realize their human potential (peak).</p>
         <p>
           This is a place where I, Coach Scott, share my learnings and help others like you reach their peak health.
-          You can expect topics on sport psychology, epigenetics, neuroscience, 
-          motor learning, health/wellness, fitness, prevention, self care, posture, mindset, debunking myths, technqiues you can apply to heal yourself and others, and so much more!  
-        </p>
+          You can expect topics as they relate to health and wellness on:</p>
+
+        <ul style="list-style-type:circle;">
+          <li>Sport Psychology - Neuroscience - Mindset</li>
+          <li>Fitness - Posture - Optimal human movement</li>
+          <li>Injury Prevention</li>
+          <li>Self Care - Healing</li>
+          <li>Orthopedic Rehabilitation</li>
+          <li>Epigenetics</li>
+          <li>Debunking myths</li>
+          <li>Proven techniques to heal yourself and others</li>
+          <li>Real stories and so much more!</li>
+        </ul> 
+
         <p>Let's go build something great and reach our peak!</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
